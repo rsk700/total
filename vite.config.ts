9986bdc3ceac34f7642c7ebddabc6b18ea24c9ae
rsk <rskbox@gmail.com>
2023-10-30 00:07:52 +0300

    update icon, copy ionicons with vite copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), viteStaticCopy({
+    targets: [{
+      src: 'node_modules/ionicons/dist/ionicons',
+      dest: 'public/'
+    }]
+  })],
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
